34f8fab925d81adbc2dd18b496d01fe9c377ccf3
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-11-23 18:35:30 -0500

    starting point of hw6

---
+++ section7/hw6assignment.rb
@@ -0,0 +1,20 @@
+# Coursera - Programming Languages (University of Washington), Homework 6
+
+# This is the only file you turn in, so do not modify the other files as
+# part of your solution.
+
+class MyPiece < Piece
+  # The constant All_My_Pieces should be declared here
+
+  # your enhancements here
+end
+
+class MyBoard < Board
+  # your enhancements here
+
+end
+
+class MyTetris < Tetris
+  # your enhancements here
+
+end
